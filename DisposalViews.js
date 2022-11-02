e42a52856f9188b86a87aa79c24c8d610a0e7c27
Brandon Liew Yi Quan <94006612+WuZhangSZD@users.noreply.github.com>
2022-11-02 23:34:42 +0800

    CSS
    Disposal and Viewer

---
+++ DisposalViews.js
@@ -0,0 +1,150 @@
+import React from 'react';
+import PlayerViews from './PlayerViews';
+
+const exports = {...PlayerViews};
+
+const sleep = (milliseconds) => new Promise(resolve => setTimeout(resolve, milliseconds));
+
+exports.Wrapper = class extends React.Component {
+  render() {
+    const {content} = this.props;
+    return (
+      <div className="Disposal">
+        <h2>Disposal (Disposal)</h2>
+        {content}
+      </div>
+    );
+  }
+}
+
+exports.SetBundleInfo = class extends React.Component {
+  render() {
+    const {parent, defaultWager, standardUnit} = this.props;
+    const bundleName = (this.state || {}).bundleName || "Unknown";
+    const boughtDate = (this.state || {}).boughtDate || "Unknown";
+    const boughtPrice= (this.state || {}).boughtPrice || 1;
+    const disposePrice = (this.state || {}).disposePrice || 1;
+    const price = (this.state || {}).price || 0;
+    return (
+      <div className='input-holder'>
+        <div className='input'>
+          <label>Bundle Name:
+            <br/>
+        <input
+          type='text'
+          placeholder= "BundleName"
+          onChange={(e) => this.setState({bundleName: e.currentTarget.value})}
+          />
+          </label>
+        </div>
+        <div className='input'>
+          <label>Bought Date:
+            <br/>
+        <input
+          type='text'
+          placeholder= "boughtDate"
+          onChange={(e) => this.setState({boughtDate: e.currentTarget.value})}
+          />
+          </label>
+        </div>
+        <div className='input'>
+          <label>Bought Price: 
+            <br/>
+        <input
+          type='number'
+          placeholder= "boughtPrice"
+          onChange={(e) => this.setState({boughtPrice: e.currentTarget.value})}
+          />
+        </label>
+        </div>
+        <div className='input'>
+          <label>Disposal Price:
+            <br/>
+        <input
+          type='number'
+          placeholder= "disposePrice"
+          onChange={(e) => this.setState({disposePrice: e.currentTarget.value})}
+        />
+        </label>
+        </div>
+        <div>
+        <button
+          onClick={() => parent.setBundle({bundleName:bundleName,boughtDate: boughtDate,boughtPrice: boughtPrice,disposePrice: reach.parseCurrency(disposePrice)})}
+        >Set bundle</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+exports.Disposing = class extends React.Component {
+  render() {
+    return (
+      <div>Disposing... please wait.</div>
+    );
+  }
+}
+exports.StartDispose = class extends React.Component {
+  render() {
+    const {parent, bundleName, bundleLocation, standardUnit} = this.props;
+    return (
+      <div>
+        <button
+          onClick={() => parent.dispose()}
+        >Start dispose</button>
+      </div>
+    );
+  }
+}
+exports.DisposeDone = class extends React.Component {
+  async copyToClipboard(button) {
+    const {ctcInfoStr} = this.props;
+    navigator.clipboard.writeText(ctcInfoStr);
+    const origInnerHTML = button.innerHTML;
+    button.innerHTML = 'Copied!';
+    button.disabled = true;
+    await sleep(1000);
+    button.innerHTML = origInnerHTML;
+    button.disabled = false;
+  }
+
+  render() {
+    const {bundleName,boughtDate,boughtPrice,disposePrice,ctcInfoStr, standardUnit} = this.props;
+    // const {boughtDate} = (this.state || {}).boughtDate || "Nothing";
+    // const {boughtPrice} = this.props.boughtPrice;
+    // const {disposePrice} = this.props.disposePrice;
+    return (
+      <div className='details-card'>
+        Your pre-loved have been registered
+        <h2>
+          Pre-loved Name 
+        </h2>
+          {bundleName}
+        <h2>
+          Pre-loved Bought-day 
+        </h2>
+          {boughtDate}
+        <h2>
+          Pre-loved bought price 
+        </h2>
+          {boughtPrice} {standardUnit}
+        <h2>
+          Pre-loved dispose price 
+        </h2>
+          {disposePrice} {standardUnit}
+        <h2>
+          ContractNumber (please record) 
+        </h2>
+        <pre className='ContractInfo'>
+          {ctcInfoStr}
+        </pre>
+        <button
+          onClick={(e) => this.copyToClipboard(e.currentTarget)}
+        >Copy to clipboard</button>
+      </div>
+    )
+  }
+}
+
+export default exports;
